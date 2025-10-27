0b23aab8586aab1698d37c6dd4fce3db20a51938
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2025-10-27 22:58:34 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <p>hey world</p>
+        <h1>hdad</h1>
+      </div>
+    </>
+  );
+}
