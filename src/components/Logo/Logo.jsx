7871afd1238c512b3f65f29bc95c5f111210e41d
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2025-11-29 20:27:08 +0200

    Changed profile picture to xmas in hero section

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import profilelogo from "../../assets/profile-logo.png";
+import profilelogo from "../../assets/profile-logo-xmas.png";
 
 export default function Logo() {
   return <img src={profilelogo} className="profile-logo"></img>;
