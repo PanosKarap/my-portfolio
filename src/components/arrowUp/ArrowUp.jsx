5d9f00407d2ac2165f789815b268e2a621f21aee
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2025-12-02 18:16:48 +0200

    Removed up arrow from phone size devices

--- src/components/arrowUp/ArrowUp.jsx
+++ src/components/arrowUp/ArrowUp.jsx
@@ -3,24 +3,22 @@ import "./ArrowUp.css";
 export default function ArrowUp() {
   const arrowUpIcon = (
     <svg
-      fill="#930000"
+      fill="var(--primary-base)"
       height="5rem"
       width="5rem"
       version="1.1"
       id="Capa_1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="-24.5 -24.5 539.00 539.00"
-      stroke="#930000"
-      strokeWidth="0.0049" // <-- FIXED
     >
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
 
       <g
         id="SVGRepo_tracerCarrier"
-        strokeLinecap="round" // <-- FIXED
-        strokeLinejoin="round" // <-- FIXED
+        strokeLinecap="round"
+        strokeLinejoin="round"
         stroke="#030303"
-        strokeWidth="11.76" // <-- FIXED
+        strokeWidth="11.76"
       >
         <g>
           <path d="M0,235.564v254.047l245-235.564l245,235.564V235.564L245,0.389L0,235.564z M459.375,417.684L245,211.549L30.625,417.684 v-169.08L245,42.827l214.375,205.777V417.684z"></path>
