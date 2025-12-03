568e53001313f4d8b8f6dd89de9659441c0dba54
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2025-12-03 16:31:11 +0200

    Changed about paragraph text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,14 +11,14 @@ export default function About() {
       <h2 className="occupation">Software Engineer</h2>
       <h1 className="name">Hello! I'm Panagiotis Karapanagiotis.</h1>
       <p>
-        I'm 20 years old and was born in Greece. I graduated from the{" "}
+        I'm 20 years old and was born in Greece. I'm in my 3rd year studying
+        Informatics in the{" "}
         <a href="https://unipi.gr" target="_blank">
           University of Piraeus
         </a>{" "}
-        with a degree in Informatics, while I was also testing and improving my
-        skills by building some <a href="#">personal projects</a> in my free
-        time. I specialize in App Development and am currently looking for job
-        opportunities to apply my knowledge and grow as a developer.
+        , while also testing and improving my skills by building some{" "}
+        <a href="#">personal projects</a> in my free time. I'm currently looking
+        for job opportunities to apply my knowledge and grow as a developer.
       </p>
       <div className="about-icon-container">
         <SocialIcon
