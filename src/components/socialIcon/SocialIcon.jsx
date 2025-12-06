b4b071ca990c5666f753d3ef305ec72ffef1ea5a
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2025-12-06 18:03:55 +0200

    Changed all svgs

--- src/components/socialIcon/SocialIcon.jsx
+++ src/components/socialIcon/SocialIcon.jsx
@@ -2,16 +2,13 @@ import "./SocialIcon.css";
 
 export default function SocialIcon({ icon, url }) {
   return (
-    <div className="social-icon-container">
-      <a
-        className="social-icon-url"
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Social profile"
-      >
-        <div className="social-icon">{icon}</div>
-      </a>
-    </div>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Social profile"
+    >
+      <div className="social-icon">{icon}</div>
+    </a>
   );
 }
